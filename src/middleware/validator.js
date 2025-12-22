726212fbf2e6ce2c75c7f099bb4603efa9617958
Alishan <alishan.n@orbics.co>
2025-12-22 19:22:57 +0500

    Added Optional

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,13 +1,17 @@
-const validator = (schema) => (req, res, next) => {    
+const validator = (schema, options = { optional: false }) => (req, res, next) => {
+
+    const { optional } = options
 
     if (!req.body || Object.keys(req.body).length === 0) {
-
-        return res.status(400).json({
-            success: false,
-            message: 'Request body is missing or empty.',
-        })
-
-    }    
+        if (optional) {
+            return next()
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: 'Request body is missing or empty.',
+            })
+        }
+    }
 
     const { value, error } = schema.validate(req.body, { abortEarly: false, stripUnknown: true });
 
