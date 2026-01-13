38f049c91f872e0c828d484714dfb8fc4edb423e
Alishan <alishan.n@orbics.co>
2026-01-13 21:43:32 +0500

    Added Feedbacks Controller

--- src/controllers/feedback.controller.js
+++ src/controllers/feedback.controller.js
@@ -1,5 +1,7 @@
-import Feedback from "../models/feedback.model.js"
 import logger from "../config/logger.js"
+import { buildPaginationResponse, getPagination } from "../helpers/pagination.js"
+import Feedback from "../models/feedback.model.js"
+import { searchRegex } from "../utils/index.js"
 
 export const addFeedback = async (req, res, next) => {
     try {
@@ -30,3 +32,60 @@ export const addFeedback = async (req, res, next) => {
         next(error)
     }
 }
+
+export const getFeedbacks = (async (req, res) => {
+    try {
+
+        const { query } = req
+        const { search } = query
+        const { skip, limit, page, page_size } = getPagination(query)
+
+        let filter = {}
+
+        let sort = {
+            createdAt: -1
+        }
+
+        let projection = {}
+
+        if (search) {
+            filter.name = searchRegex(search)
+        }
+
+        const feedbacks = await Feedback.find(filter, projection, options).populate({ path: "user", select: "name email picture" }).sort(sort).skip(skip).limit(limit)
+        const total = await Feedback.countDocuments(filter)
+
+        logger.info(`Feedbacks listing fetched`)
+
+        return res.status(200).json({
+            success: true,
+            message: 'Feedbacks fetched successfully.',
+            ...buildPaginationResponse(feedbacks, total, page, page_size),
+        })
+
+    } catch (e) {
+        logger.error(`Get Feedbacks Error: ${error.message}`)
+        next(error)
+    }
+})
+
+const getFeedbackById = (async (req, res) => {
+    try {
+
+        let id = req.params.id
+
+        let feedback = await Feedback.findById(id).populate({ path: "user", select: "role" })
+
+        return res.status(200).send({
+            success: true,
+            data: feedback
+        })
+
+    } catch (e) {
+        console.log("Error Message :: ", e)
+        res.status(400).send({
+            success: false,
+            message: e.message
+        })
+    }
+})
--- src/routes/feedback.route.js
+++ src/routes/feedback.route.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { addFeedback } from '../controllers/feedback.controller.js'
+import { addFeedback, getFeedbacks } from '../controllers/feedback.controller.js'
 import { CREATE_FEEDBACK_VALIDATOR } from '../helpers/validators.js'
-import { OptionalAuthVerifier } from '../middleware/auth.middleware.js'
+import { AuthVerifier, OptionalAuthVerifier } from '../middleware/auth.middleware.js'
 import validator from '../middleware/validator.js'
 
 const router = express.Router()
+
+router.get('/get', AuthVerifier, getFeedbacks)
 
 router.post('/create', OptionalAuthVerifier, validator(CREATE_FEEDBACK_VALIDATOR), addFeedback)
 
