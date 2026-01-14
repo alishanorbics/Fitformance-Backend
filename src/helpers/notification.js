06abf37323c06d0449587c36b9db1c2e5aee510e
Alishan <alishan.n@orbics.co>
2026-01-15 00:08:32 +0500

    Created helper for notification

---
+++ src/helpers/notification.js
@@ -0,0 +1,40 @@
+import logger from "../config/logger"
+import Notification from "../models/notification.model.js"
+import User from "../models/user.model.js"
+import { ROLES } from "../utils"
+
+export const sendNotification = async ({ title, message, user_ids, metadata, admin = false }) => {
+
+    if (!title || !message || !user_ids) {
+        throw new Error('Invalid parameters for send notification.')
+    }
+
+    if (admin) {
+        const admins = await User.find({ role: ROLES.ADMIN, active: true }).select('_id')
+        user_ids = admins.map(item => item._id)
+    }
+
+    const recipients = user_ids.map(item => ({
+        user: item,
+        read: false
+    }))
+
+    if (!recipients.length) {
+        logger.warn('No recipients found for notification:', title)
+        return null
+    }
+
+    const notification = new Notification({
+        title,
+        message,
+        recipients,
+        metadata
+    })
+
+    await notification.save()
+
+    logger.info(`Notification sent to ${user_ids.length} user(s)`)
+
+    return notification
+
+}
