66b11055c847e520b11e4904eeaa2bbd6e1b45d7
Alishan <alishan.n@orbics.co>
2026-01-15 00:48:15 +0500

    Updated

--- src/app.js
+++ src/app.js
@@ -26,7 +26,11 @@ app.use(requestLogger)
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-app.use('/uploads', express.static(path.join(__dirname, "uploads")))
+app.use('/uploads', express.static(path.join(__dirname, "uploads")), {
+    setHeaders: (res, path) => {
+        res.set('Access-Control-Allow-Origin', '*');
+    }
+})
 app.use(`/${process.env.APP_NAME}/v1/api`, routes)
 app.get('/health', (req, res) => res.status(200).send('OK'))
 
