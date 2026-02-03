4f4faef38b34f1d1acdab67d045d6bcd6c7f91da
Alishan <alishan.n@orbics.co>
2026-02-03 15:25:03 +0500

    Updated Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,22 +4,24 @@ const router = express.Router()
 
 import auth_routes from './authentication.route.js'
 import user_routes from './user.route.js'
+import rehab_routes from './rehab.route.js'
+import package_routes from './package.route.js'
+
 import bet_routes from './bet.route.js'
-import transaction_routes from './transaction.route.js'
 import wallet_routes from './wallet.route.js'
 import feedback_routes from './feedback.route.js'
 import general_routes from './general.route.js'
-import dispute_routes from './dispute.route.js'
 import notification_routes from './notification.route.js'
 
 router.use('/auth', auth_routes)
 router.use('/user', user_routes)
+router.use('/rehab', rehab_routes)
+router.use('/package', package_routes)
+router.use('/notification', notification_routes)
+
 router.use('/bet', bet_routes)
-router.use('/transaction', transaction_routes)
 router.use('/wallet', wallet_routes)
 router.use('/feedback', feedback_routes)
 router.use('/general', general_routes)
-router.use('/dispute', dispute_routes)
-router.use('/notification', notification_routes)
 
 export default router
