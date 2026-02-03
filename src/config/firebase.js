72770d52ad9b9b1a8030e97aad97b91a58b1de11
Alishan <alishan.n@orbics.co>
2026-02-03 15:31:21 +0500

    Updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ import firebase from 'firebase-admin'
 import logger from './logger.js'
 
 const require = createRequire(import.meta.url)
-const service_account = require('./fitformance-4356b-c157f670c84e.json')
+const service_account = require('./fitformance.json')
 
 firebase.initializeApp({
     credential: firebase.credential.cert(service_account),
