0ca1d540dec1ff68957c454cce6ed787f1910850
alishanorbics <alishan.n@orbics.co>
2026-05-08 20:23:14 +0500

    Refactor upload middleware to use dynamic file size limits based on folder name

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -1,6 +1,12 @@
 import multer from 'multer'
 import path from 'path'
 
+const limits = {
+    rehab: 100 * 1024 * 1024,
+    default: 10 * 1024 * 1024
+}
+
+
 const makeStorage = (folder_name = '') =>
     multer.diskStorage({
         destination: (req, file, cb) => {
@@ -32,7 +38,7 @@ const upload = (folder_name = '') => {
 
     return multer({
         storage,
-        limits: { fileSize: 10 * 1024 * 1024 },
+        limits: { fileSize: limits[folder_name] || limits.default },
         fileFilter: (req, file, cb) => {
             cb(null, true)
         },
